de0f5b05d85ee5229d4a48f1a84d93577ca9fc30
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-06-08 13:47:27 -0500

    can now add user and get user by username

--- BizarreBazaar/DataAccess/UserRepo.cs
+++ BizarreBazaar/DataAccess/UserRepo.cs
@@ -41,5 +41,68 @@ namespace BizarreBazaar.DataAccess
                 return result;
             }
         }
+
+        public User GetUserByUserName(string userName)
+        {
+            var sql = @"
+                    select *
+                    from [user]
+                    where UserName = @userName ";
+
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var parameters = new {UserName = userName};
+                var result = db.QueryFirstOrDefault<User>(sql, parameters);
+                return result;
+            }
+        }
+
+        public User Add(User user)
+        {
+            var sql = $@"
+            insert into [user](FirstName, LastName, AcctCreated, AcctActive, isSeller, UserName, Email, [Password])
+            output inserted. *
+            Values(@FirstName, @LastName, GETDATE(), @AcctActive, @isSeller, @UserName, @Email, @Password )";
+
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var result = db.QueryFirstOrDefault<User>(sql, user);
+                return result;
+            }
+        }
+
+
+        public IEnumerable<User> GetCompletedOrdersByUserId(int uid)
+        {
+            var sql = @"
+                select 
+                [user].ID,
+                [order].id as orderId, 
+                [user].username as Seller, 
+                [order].userId, 
+                product.title, 
+                product.imageurl, 
+                [order].invoicedate as dateordered, 
+                product.price, 
+                lineitem.quantity, 
+                paymenttype.[name] as paymenttype, 
+                product.price * lineitem.quantity as producttotal
+                from lineitem
+                join product on product.id = productId
+                join [user] on [user].id = product.userId
+                join [order] on [order].id = orderId
+                join payment on payment.id = [order].paymentid
+                join paymenttype on paymenttype.id = payment.paymenttypeId
+                where iscomplete = 1 and [order].userId = @uid
+                order by [order].id
+                ";
+
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var parameters = new { Uid = uid };
+                var result = db.Query<User>(sql, parameters);
+                return result;
+            }
+        }
     }
 }
