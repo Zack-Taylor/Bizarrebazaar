2e38bcf63cd72932a632f63b4569807c83a12872
Maria <mariabrock414@gmail.com>
2020-06-08 20:42:23 -0500

    can view a sellers products

--- BizarreBazaar/Controllers/ProductController.cs
+++ BizarreBazaar/Controllers/ProductController.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BizarreBazaar.DataAccess;
+using BizarreBazaar.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BizarreBazaar.Controllers
 {
-    public class ProductController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
     {
+        ProductRepo _repository;
+
+        public ProductController(ProductRepo repository)
+        {
+            _repository = repository;
+        }
+
+        //api/product/{userId}/
+        [HttpGet("{UserId}/")]
+        public IActionResult GetAllSellersProducts(int UserId)
+        {
+            var products = _repository.GetAllSellersProducts(UserId);
+            var isEmpty = !products.Any();
+            if (isEmpty)
+            {
+                return NotFound("Bummer man. This user has no products");
+            }
+
+            return Ok(products);
+
+        }
     }
 }
--- BizarreBazaar/DataAccess/ProductRepo.cs
+++ BizarreBazaar/DataAccess/ProductRepo.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using BizarreBazaar.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +12,26 @@ namespace BizarreBazaar.DataAccess
 {
     public class ProductRepo
     {
+        string ConnectionString;
+
+        public ProductRepo(IConfiguration config)
+        {
+            ConnectionString = config.GetConnectionString("BizarreBazaar");
+        }
+
+        public IEnumerable<Product> GetAllSellersProducts(int sellersId)
+        {
+            var sql = @"select * from Product
+                        where UserId = @sellersId";
+
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var parameters = new { SellersId = sellersId };
+                var result = db.Query<Product>(sql, parameters);
+                return result;
+            }
+        }
+
+
     }
 }
