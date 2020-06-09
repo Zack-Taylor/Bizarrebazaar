83d060e08abe5a7d5419554a27f9024c80aeeace
Jbkey1983 <jbk_1_tn@yahoo.com>
2020-06-08 20:36:03 -0500

    WIP

--- BizarreBazaar/Controllers/ProductTypeController.cs
+++ BizarreBazaar/Controllers/ProductTypeController.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using BizarreBazaar.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BizarreBazaar.Controllers
 {
-    public class ProductTypeController
+    public class ProductTypeController : ControllerBase
     {
+
+        [HttpGet("{category}")]
+        public IActionResult GetProductByProductType(string category)
+        {
+            var repo = new ProductTypeRepo();
+            var productCategories = repo.GetProductByProductType(category);
+            return Ok(productCategories);
+        }
     }
 }
--- BizarreBazaar/DataAccess/ProductTypeRepo.cs
+++ BizarreBazaar/DataAccess/ProductTypeRepo.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BizarreBazaar.Models;
+using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,25 @@ namespace BizarreBazaar.DataAccess
 {
     public class ProductTypeRepo
     {
+        const string ConnectionString = "Server=localhost;Database=CandyMarket;Trusted_Connection=True;";
+        public ProductType GetProductByProductType(string category)
+        {
+            var sql = @"select producttype.[name], count(*) as productcount
+                        from product
+                            join producttype on product.producttypeid = producttype.id
+                                group by producttype.[name]
+                                    ";
+
+            var parameters = new
+            {
+                Category = category
+            };
+
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var summary = db.QueryFirstOrDefault<ProductType>(sql, parameters);
+                return summary;
+            }
+        }
     }
 }
