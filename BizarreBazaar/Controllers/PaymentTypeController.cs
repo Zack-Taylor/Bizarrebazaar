9aa090b551b50b567959ba5c4b7344644b90560e
Jbkey1983 <jbk_1_tn@yahoo.com>
2020-06-09 13:16:49 -0500

    Can Get All Payment Types

--- BizarreBazaar/Controllers/PaymentTypeController.cs
+++ BizarreBazaar/Controllers/PaymentTypeController.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using BizarreBazaar.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BizarreBazaar.Controllers
 {
-    public class PaymentTypeController
+    [Route("api/paymenttype")]
+    [ApiController]
+    public class PaymentTypeController : ControllerBase
     {
+        PaymentTypeRepo _repository;
+
+        public PaymentTypeController(PaymentTypeRepo repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPaymentTypes()
+        {
+            var allPaymentTypes = _repository.GetAllPaymentTypes();
+
+            return Ok(allPaymentTypes);
+        }
+
+
+
     }
 }
--- BizarreBazaar/DataAccess/PaymentTypeRepo.cs
+++ BizarreBazaar/DataAccess/PaymentTypeRepo.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using BizarreBazaar.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +11,19 @@ namespace BizarreBazaar.DataAccess
 {
     public class PaymentTypeRepo
     {
+        string ConnectionString;
+
+        public PaymentTypeRepo(IConfiguration config)
+        {
+            ConnectionString = config.GetConnectionString("BizarreBazaar");
+        }
+
+        public IEnumerable<PaymentType> GetAllPaymentTypes()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                return db.Query<PaymentType>("select * from PaymentType");
+            }
+        }
     }
 }
