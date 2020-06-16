8419655f3f1db31bce548648adc58975072c7575
Ivan Phelps <ivanniothemannio@gmail.com>
2020-06-15 19:10:21 -0500

    added sql script folder and imageUrl updates

---
+++ SQL_Scripts/Ivan_image_urls.sql
@@ -0,0 +1,76 @@
+update [user]
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/userImages/paula_jones.png'
+where id = 4
+
+update [user]
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/userImages/link_betweenworldsington.png'
+where id = 5
+
+update [user]
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/userImages/doom_slayer.png'
+where id = 6
+
+update [user]
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/userImages/gordon_freeman.png'
+where id = 7
+
+update [user]
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/userImages/mario_mario.png'
+where id = 8
+
+update [user]
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/userImages/bowser_koopa.png'
+where id = 9
+
+update [user]
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/userImages/mayor_lewis.png'
+where id = 10
+
+update [user]
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/userImages/professor_oak.png'
+where id = 11
+
+update [user]
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/userImages/banjo_kazooie.png'
+where id = 12
+
+update product
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/franklin_badge.png'
+where id = 25
+
+update product
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/master_sword.png'
+where id = 26
+
+update product
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/BFG_9000.png'
+where id = 27
+
+update product
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/fire_flower.png'
+where id = 28
+
+update product
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/blue_shell.png'
+where id = 29
+
+update product
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/gravity_gun.png'
+where id = 30
+
+update product
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/stardrop.png'
+where id = 31
+
+update product
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/master_ball.png'
+where id = 32
+
+update product
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/power_moon.png'
+where id = 33
+
+update product
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/ice_key.png'
+where id = 34
+
