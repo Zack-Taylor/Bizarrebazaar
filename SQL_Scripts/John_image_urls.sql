84dd287df5d281d1a0964d083f5d9f815811c39d
jbkey2015 <54037058+jbkey2015@users.noreply.github.com>
2020-06-15 19:19:16 -0500

    Add files via upload

---
+++ SQL_Scripts/John_image_urls.sql
@@ -0,0 +1,43 @@
+update [user]
+set imageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/userImages/Indiana%20Jones.png'
+where id = 2
+
+update product
+set ImageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/Boot-of-Cortez-Gold-Nugget.png'
+where id = 15
+
+update product
+set ImageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/Magna%20carta.png'
+where id = 16
+
+update product
+set ImageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/mona%20lisa.png'
+where id = 17
+
+update product
+set ImageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/largest%20pearl.png'
+where id = 18
+
+update product
+set ImageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/t-rex.png'
+where id = 19
+
+update product
+set ImageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/glass%20slipper.png'
+where id = 20
+
+update product
+set ImageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/the%20ark%20of%20the%20covenant.png'
+where id = 21
+
+update product
+set ImageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/hope%20diamond.png'
+where id = 22
+
+update product
+set ImageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/the%20starry%20night.png'
+where id = 23
+
+update product
+set ImageUrl = 'https://raw.githubusercontent.com/Zack-Taylor/Bizarrebazaar/master/bizarrebazaar.ui/src/assets/productImages/buddhas%20tooth.png'
+where id = 24
