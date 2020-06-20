a936e78cca7f363fd74dfedbcfb92b7b3b65ab44
Ivan Phelps <ivanniothemannio@gmail.com>
2020-06-19 23:21:37 -0500

    product page looking nice, getting product type also

--- bizarrebazaar.ui/src/components/pages/ProductDetail/ProductDetail.js
+++ bizarrebazaar.ui/src/components/pages/ProductDetail/ProductDetail.js
@@ -1,39 +1,61 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 import productData from '../../../helpers/data/productData';
+import productTypeData from '../../../helpers/data/productTypeData';
 import './ProductDetail.scss';
 
 class ProductDetail extends React.Component {
   state = {
     product: {},
-  }
+    productType: {},
+  };
 
   componentDidMount() {
-    this.setProductToState(26);
+    this.getProductandProductType(30);
   }
 
-    setProductToState = (productId) => {
-      productData.getProductById(productId)
-        .then((result) => this.setState({ product: result.data }))
-        .catch((error) => console.error('error getting that product', error));
-    }
+  getProductandProductType = (productId) => {
+    productData
+      .getProductById(productId)
+      .then((result) => this.setProductAndProductTypeToState(result.data))
+      .catch((error) => console.error('error getting that product, ', error));
+  };
 
-    render() {
-      const { product } = this.state;
-      return (
-            <Container maxWidth="md">
-                <h3 className="product-title-header">{product.title}</h3>
-                <Divider />
-                <img className="product-image" src={product.imageUrl} alt={product.title}></img>
-                <ul>
-                    <li>{product.description}</li>
-                    <li>Price: {product.price}</li>
-                    <li>Quantity in stock: {product.quantity}</li>
-                </ul>
-          </Container>
-      );
-    }
+  setProductAndProductTypeToState = (product) => {
+    this.setState({ product });
+    productTypeData
+      .getProductTypeById(product.productTypeId)
+      .then((result) => this.setState({ productType: result.data }))
+      .catch((error) => console.error('error getting that product type, ', error));
+  };
+
+  render() {
+    const { product, productType } = this.state;
+    return (
+      <div className="image-and-details-container">
+        <div className="product-image-div">
+          <img
+            className="product-image"
+            src={product.imageUrl}
+            alt={product.title}
+          ></img>
+        </div>
+        <div className="product-details-div">
+          <h1 className="product-title">{product.title}</h1>
+          <sub className="filed-under">Filed under: {productType.name}</sub>
+          <p className="description">{product.description}</p>
+          <div className="price-and-button">
+          <h1 className="price">${product.price}</h1>
+            <Button variant="contained"
+                  color="primary"
+                  size="large"
+                  className="add-to-cart-button"
+                  > Add To Cart</Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ProductDetail;
