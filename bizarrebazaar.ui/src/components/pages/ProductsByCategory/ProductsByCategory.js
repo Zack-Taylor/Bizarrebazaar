ad30dd8c5cfcbd84a91576f4a7dce0439e8f503f
Jbkey1983 <jbk_1_tn@yahoo.com>
2020-06-20 12:59:47 -0500

    categories show on page and getting product data back

--- bizarrebazaar.ui/src/components/pages/ProductsByCategory/ProductsByCategory.js
+++ bizarrebazaar.ui/src/components/pages/ProductsByCategory/ProductsByCategory.js
@@ -1,11 +1,43 @@
-import './ProductsByCategory.scss';
 import React from 'react';
+import productsByCategory from '../../../helpers/data/getProductsByCategoryData';
+import productTypes from '../../../helpers/data/productTypeData';
+import './ProductsByCategory.scss';
+import ProductDetail from '../ProductDetail/ProductDetail';
 
 class ProductsByCategory extends React.Component {
+  state = {
+    productType: {},
+    product: [],
+  }
+
+  getProductData = (ProductTypeId) => {
+    productsByCategory.getProductsByCategory(ProductTypeId)
+      .then((products) => this.setState({ products }))
+      .catch((err) => console.error('error in get products by category', err));
+  }
+
+  componentDidMount() {
+    const { productTypeId } = this.props.match.params;
+    productTypes.getProductTypeById(productTypeId)
+      .then((response) => {
+        this.setState({ productType: response.data });
+        this.getProductData(productTypeId);
+      })
+      .catch((err) => console.error('error in get single product type', err));
+  }
+
   render() {
+    const {
+      productType,
+      product,
+    } = this.state;
+
     return (
-      <div>
-        <h1>ProductsByCategory</h1>
+      <div className="ProductsByCategory">
+        <h1 className="productTypeName">{productType.name}</h1>
+        <div className="products d-flex flex-wrap">
+          { this.state.product.map((p) => <ProductDetail key={product.productId} product={p}/>)}
+        </div>
       </div>
     );
   }
--- bizarrebazaar.ui/src/helpers/data/getProductsByCategoryData.js
+++ bizarrebazaar.ui/src/helpers/data/getProductsByCategoryData.js
@@ -1,10 +1,29 @@
 import axios from 'axios';
 import constants from '../constants.json';
 
-const getProductsByCategory = (id) => new Promise((resolve, reject) => {
-  axios.get(`${constants.baseUrl}/product/productTypeId/${id}`)
-    .then((result) => resolve(result.data))
-    .catch((error) => reject(error));
+// const getProductsByCategory = (id) => new Promise((resolve, reject) => {
+//   axios.get(`${constants.baseUrl}/product/productTypeId/${id}`)
+//     .then((result) => resolve(result.data))
+//     .catch((error) => reject(error));
+// });
+
+const getProductsByCategory = (productTypeId) => new Promise((resolve, reject) => {
+  axios.get(`${constants.baseUrl}/product/productTypeId/${productTypeId}`)
+    .then((result) => {
+      const allProductsObj = result.data;
+      const products = [];
+      if (allProductsObj != null) {
+        Object.keys(allProductsObj).forEach((productId) => {
+          const newProduct = allProductsObj[productId];
+          newProduct.id = productId;
+          products.push(newProduct);
+        });
+      }
+      resolve(products);
+    })
+    .catch((err) => {
+      reject(err);
+    });
 });
 
 export default { getProductsByCategory };
