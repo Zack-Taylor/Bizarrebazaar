743058182de58ff4102f554c8c7a8f7dd451bf7c
Ivan Phelps <ivanniothemannio@gmail.com>
2020-06-25 00:42:22 -0500

    correct info in state

--- bizarrebazaar.ui/src/components/pages/Shop/Shop.js
+++ bizarrebazaar.ui/src/components/pages/Shop/Shop.js
@@ -3,41 +3,35 @@ import React from 'react';
 import Divider from '@material-ui/core/Divider';
 import productData from '../../../helpers/data/productData';
 import productTypeData from '../../../helpers/data/productTypeData';
-import ProductCard from '../../shared/ProductCard/ProductCard';
 
 class Shop extends React.Component {
   state = {
-    typesAndTopThree: [],
+    productsWithType: [],
   }
 
-  returnTopThree = (productTypeId) => {
-    productData.shopTopThree(productTypeId)
-      .then((response) => response.data)
-      .catch((error) => console.error('error getting top three, ', error));
+  setDataToState = (productTypes) => {
+    productTypes.map((pt) => {
+      productData.shopTopThree(pt.id)
+        .then((response) => this.setState({ productsWithType: [...this.state.productsWithType, { productType: `${pt.name}`, products: response.data }] }))
+        .catch((error) => console.error('error getting top three, ', error));
+    });
+  }
+
+  getProductsWithTypes = () => {
+    productTypeData.getAllProductTypes()
+      .then((response) => this.setDataToState(response.data))
+      .catch((error) => console.error('error getting product types: ', error));
   }
 
   componentDidMount() {
-    productTypeData.getAllProductTypes()
-      .then((response) => {
-        const objects = [];
-        response.data.map((prodType) => {
-          const topThree = this.returnTopThree(prodType.id);
-          const typeAndThree = {
-            typeName: prodType.name,
-            topThreeProducts: topThree,
-          };
-          objects.concat(typeAndThree);
-          this.setState({ typesAndTopThree: objects });
-        });
-      })
-      .catch((err) => console.error('error getting product types', err));
+    this.getProductsWithTypes();
   }
 
   render() {
-    const { typesAndTopThree } = this.state;
+    const { productsWithType } = this.state;
     return (
       <div className="products-outer-container">
-        <h1 className="product-type-name">{typesAndTopThree[1]}</h1>
+        <h1 className="product-type-name"></h1>
         <Divider></Divider>
         {/* <div className="product-card-container">
           {products == null ? [] : products.map((product) => <ProductCard key={product.id} product={product} />) }
